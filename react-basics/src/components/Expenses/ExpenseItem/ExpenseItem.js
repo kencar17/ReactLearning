20fc41f7f51c7b6e1bd488c0b0cc0f9aa188de4c
carmichaelk3@mymacewan.ca <carmichaelk3@mymacewan.ca>
2023-04-09 22:05:45 -0600

    chore start playing with react state

--- react-basics/src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ react-basics/src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../../UI/Card/Card';
 import './ExpenseItem.css';
@@ -5,13 +7,20 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }

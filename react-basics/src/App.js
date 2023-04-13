db26bb23c20070b87d85f07b8fa2ef39428b6e49
carmichaelk3@mymacewan.ca <carmichaelk3@mymacewan.ca>
2023-04-12 22:21:36 -0600

    chore forms and state handling practice

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,4 +1,5 @@
 import Expense from "./components/Expenses/Expense/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -26,6 +27,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expense expenses={expenses}/>
     </div>
   );
